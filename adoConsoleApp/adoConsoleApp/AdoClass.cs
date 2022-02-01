87b1f513e1074ceb304620e78c9b7fbc4509fb96
Shireesh Kumar <33771300+shireeshglug@users.noreply.github.com>
2022-02-01 12:56:41 +0530

    Add files via upload

---
+++ adoConsoleApp/adoConsoleApp/AdoClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace adoConsoleApp
+{
+    class AdoClass
+    {
+
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader reader;
+
+        public AdoClass()
+        {
+            try
+            {
+                con = new SqlConnection("Data Source=localhost ; Initial Catalog=ShoppingDB ; Integrated Security=True");
+                con.Open();
+                cmd = new SqlCommand();
+                cmd.Connection = con;
+                Console.WriteLine("Connection Done");
+                cmd.CommandText = "select * from Customer";
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader[0].ToString() + " " + reader[1].ToString());
+                }
+                con.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+    }
+}
---
+++ adoConsoleApp/adoConsoleApp/DisconnectedADO.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+namespace adoConsoleApp
+{
+    class DisconnectedADO
+    {
+        DataSet ds = new DataSet();
+        DataTable dataTable;
+        SqlDataAdapter buildingAdapter;
+        SqlCommandBuilder commandBuilder;
+        SqlConnection con;
+        public DisconnectedADO()
+        {
+            con = new SqlConnection();
+            con.ConnectionString = "Data Source = localhost; Initial Catalog = ShoppingDB; Integrated Security = True";
+            buildingAdapter = new SqlDataAdapter("select * from building", con);
+
+            commandBuilder = new SqlCommandBuilder(buildingAdapter);
+
+            buildingAdapter.Fill(ds, "building");
+            //retrive and fill data to the dataset object
+            ds.Tables["building"].PrimaryKey = new DataColumn[] { ds.Tables["building"].Columns[0] };
+            dataTable = ds.Tables["building"];
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Console.WriteLine(row[0].ToString() + " " + row[1].ToString() + " " + row[2].ToString());//first column data
+            }
+        }
+
+        public void AddBuilding()
+        {
+            DataRow dr = dataTable.NewRow();
+            dr[0] = 3;
+            dr[1] = "sumedha";
+            dr[2] = 23044;
+            dataTable.Rows.Add(dr);
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Console.WriteLine(row[0].ToString() + " " + row[1].ToString() + " " + row[2].ToString());//first column data
+
+            }
+        }
+        public void save()
+        {
+            buildingAdapter.Update(ds,"building");
+        }
+
+        public void Update()
+        {
+            DataRow dr = dataTable.Rows.Find(2);
+            dr[1] = "keerthi";
+
+        }
+
+        public void Display()
+        {
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Console.WriteLine(row[0].ToString() + " " + row[1].ToString() + " " + row[2].ToString());
+            }
+        }
+
+        public void Delete()
+        {
+            DataRow dr = dataTable.Rows.Find(2);
+            dr.Delete();
+        }
+    }
+}
+
